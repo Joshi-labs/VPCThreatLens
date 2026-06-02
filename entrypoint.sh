1f2ea7978c8cdbf6e58e9b28d9455634d90a63f4
VP Joshi <vishwashmax@gmail.com>
2026-06-02 18:56:50 +0530

    fix [prod]

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ python embed_events.py
 
 # 2. Start the FastAPI server
 echo "Step 2: Starting FastAPI server..."
-exec uvicorn server.py:app --host 0.0.0.0 --port 8000
+exec uvicorn server:app --host 0.0.0.0 --port 8000
